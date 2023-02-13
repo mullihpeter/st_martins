802d246ae1092abb4f7c6b0fd61e44a458c6743f
muli <muli.peter121@gmail.com>
2023-02-14 00:34:49 +0300

    adding libmagic

--- st_martins/school_media/models.py
+++ st_martins/school_media/models.py
@@ -1,20 +1,19 @@
 from django.utils import timezone
 from cloudinary_storage.storage import VideoMediaCloudinaryStorage, MediaCloudinaryStorage
-from cloudinary_storage.validators import validate_video
+# from cloudinary_storage.validators import validate_video
 from django.db import models
 from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 class photos(models.Model):
     title = models.CharField(max_length=200, blank=True)
-    image = models.ImageField(upload_to='images/', blank=True, storage=MediaCloudinaryStorage(),
-                              validators=[])
+    image = models.ImageField(upload_to='images/', blank=True, storage=MediaCloudinaryStorage(), )
     description = models.TextField(max_length=500, blank=True)
 
 
 class Videos(models.Model):
     title = models.CharField(max_length=200, blank=True)
-    video = models.FileField(upload_to='videos/', blank=True, storage=VideoMediaCloudinaryStorage(),
-                              validators=[validate_video])
+    video = models.FileField(upload_to='videos/', blank=True, storage=VideoMediaCloudinaryStorage())
     description = models.TextField(max_length=500, blank=True)
     date = models.DateTimeField(auto_now_add=True)
